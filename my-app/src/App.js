3064e46eedc2e35aef7ea16202a4c574457cd88b
geekMe82 <akhalifa1982@gmail.com>
2020-03-10 23:24:43 +0300

    myapp

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+
+
+        <
+        div className = "App" >
+        iuyuyiuyiuyiu
+
+        <
+        /div>);
+    }
+
+    export default App;
